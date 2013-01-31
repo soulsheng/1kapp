40bf9659ee165fcdabcc32079cc14c1db30841cc
study <1806597655@qq.com>
2013-01-31 11:30:14 +0800

    默认链接，在新页面打开

--- soulsheng/wp-content/themes/pinboard/header.php
+++ soulsheng/wp-content/themes/pinboard/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<base target="_blank">
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php wp_title(); ?></title>
